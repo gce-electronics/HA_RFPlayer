d063e1f3c6a5e2c5bd755a9bb4ddbd757d688659
Doubledom45 <97252459+Doubledom45@users.noreply.github.com>
2022-06-13 22:21:27 +0100

    Update rfpparser.py
    Mise à jour version Béta

--- custom_components/rfplayer/rflib/rfpparser.py
+++ custom_components/rfplayer/rflib/rfpparser.py
@@ -16,13 +16,37 @@ PACKET_FIELDS = {
     "dtc": "detector",
     "sta": "status",
     "sen": "sensor",
+    "tmp": "temperature",
+    "temp": "SET_TEMPERATURE",
+    "hyg": "hygrometry",
+    "nrj": "energy",
+    "pow": "power",
+    "pw1": "P1",
+    "pw2": "P2",
+    "pw3": "P3",
 }
 
 UNITS = {
     "bat": None,
     "cmd": None,
-    "detector": None,
+    "dtc": None,
+    "hyg": "%",
+    "hygrometry": "%",
+    "nrj": "Wh",
+    "energy": "Wh",
+    "pow": "W",
+    "power": "W",
+    "pw1": "W",
+    "p1": "W",
+    "pw2": "W",
+    "p2": "W",
+    "pw3": "W",    
+    "p3": "W", 
+    "sen": None,
     "sta": None,
+    "tmp": "°C",
+    "temperature": "°C",
+#####
 }
 
 DTC_STATUS_LOOKUP = {
@@ -30,6 +54,14 @@ DTC_STATUS_LOOKUP = {
     "2": "open",
     "8": "alive",
     "16": "assoc",
+    "18": "test",
+
+#    Pour intégration future EDISIO
+    # "9": "open",
+    # "96": "off",
+    # "97": "hors_gel",
+    # "98": "eco",
+    # "99": "confort",
 }
 
 VALUE_TRANSLATION = cast(
@@ -78,8 +110,9 @@ def decode_packet(packet: str) -> list:
         data["message"] = packet.replace("ZIA--", "")
         return [data]
 
-    # Protocols
-    message = json.loads(packet.replace("ZIA33", ""))["frame"]
+ 
+    if packet.startswith("ZIA33"):
+        message = json.loads(packet.replace("ZIA33", ""))["frame"]
     data["protocol"] = message["header"]["protocolMeaning"]
 
     if data["protocol"] in ["BLYSS", "CHACON", "JAMMING"]:
@@ -87,8 +120,24 @@ def decode_packet(packet: str) -> list:
         data["command"] = message["infos"]["subType"]
         data["state"] = message["infos"]["subTypeMeaning"]
         packets_found.append(data)
-    elif data["protocol"] in ["OREGON"]:
-        data["id"] = message["infos"]["id_PHY"]
+
+#    """Decode packet X2D ajout @jaroslawp."""
+#   Voir https://github.com/gce-electronics/HA_RFPlayer/pull/12
+
+    elif data["protocol"] in ["X2D"]:
+        data["id"] = message["infos"]["id"]
+        if message["infos"]["subTypeMeaning"] == 'Detector/Sensor':
+          value = VALUE_TRANSLATION['detector'](message["infos"]["qualifier"]) 
+          data["command"] = value
+          data["state"] = value
+        else:
+          data["command"] = message["infos"]["subTypeMeaning"]
+          data["state"] = message["infos"]["qualifier"]
+        packets_found.append(data)
+#        
+
+    elif data["protocol"] in ["OREGON", "OWL"]:
+        data["id"] = message["infos"]["adr_channel"]
         data["hardware"] = message["infos"]["id_PHYMeaning"]
         for measure in message["infos"]["measures"]:
             measure_data = data.copy()
@@ -96,12 +145,50 @@ def decode_packet(packet: str) -> list:
             measure_data["state"] = measure["value"]
             measure_data["unit"] = measure["unit"]
             measure_data["type"] = measure["type"]
+            measure_data["id"]  = message["infos"]["adr_channel"]
+            measure_data["id"] += measure_data["type"]
             packets_found.append(measure_data)
+
     elif data["protocol"] in ["EDISIO"]:
         data["id"] = message["infos"]["id"]
         data["hardware"] = message["infos"]["infoMeaning"]
-        data["command"] = message["infos"]["subType"]
-        data["state"] = message["infos"]["subType"]
+        data["state"] = message["infos"]["subTypeMeaning"]
+        data["type"] = message["infos"]["subTypeMeaning"]
+        if data["state"] in ["SET_TEMPERATURE"]:
+            data["command"] = message["infos"]["add0"]
+            data["unit"] = "°C"
+        elif data["state"] in ["TOGGLE"]:
+            data["command"] = message["infos"]["qualifier"]
+        elif data["state"] in ["DIM-A"]:
+            data["command"] = message["infos"]["qualifier"]
+        else:
+            data["command"] = message["infos"]["subType"]
+            data["state"] = True #ajout pour state vide
+        packets_found.append(data)
+
+    elif data["protocol"] in ["RTS", "VISONIC"]:
+        data["id"] = message["infos"]["id"]
+        data["state"] = message["infos"]["subTypeMeaning"]
+        data["command"] = message["infos"]["qualifierMeaning"]["flags"]
+#    """Modification retour, voir pour supprimer [] ou se servir du qualifier et de translation 'detector'."""  
+        if data["command"] == ['Down/Off']:
+            data["command"] = "Down/Off"
+        elif data["command"] == ['My']:
+            data["command"] = "MY"
+        elif data["command"] == ['Up/On']:
+            data["command"] = "Up/On"
+        elif data["command"] == ['Assoc']:
+            data["command"] = "Assoc"
+        elif data["command"] == ['Tamper','Alarm','LowBatt']:
+            data["command"] = "Tamper, Alarm, LowBatt"
+        elif data["command"] == ['Tamper','Alarm','LowBatt','Supervisor/Alive']:
+            data["command"] = "Tamper, Alarm, LowBatt, Supervisor/Alive"
+        elif data["command"] == ['LowBatt','Supervisor/Alive']:
+            data["command"] = "LowBatt, Supervisor/Alive"
+        elif data["command"] == ['button/command']:
+            data["command"] = "button/command"
+        packets_found.append(data)
+
     else:
         data["id"] = message["infos"].get("id")
         data["command"] = message["infos"].get("subType")
@@ -118,6 +205,8 @@ def encode_packet(packet: PacketType) -> str:
         return f"ZIA++{command} {protocol} ID {packet['id']}"
     if "address" in packet:
         return f"ZIA++{command} {protocol} {packet['address']}"
+    else:
+        return f"ZIA++{command} {protocol}"
     raise Exception("No ID or Address found")
 
 
@@ -134,6 +223,72 @@ def serialize_packet_id(packet: PacketType) -> str:
         )
     )
 
+
+def deserialize_packet_id(packet_id: str) -> Dict[str, str]:
+    """Deserialize packet id."""
+    if packet_id == "rfplayer":
+        return {"protocol": "unknown"}
+
+    if packet_id == "ZIA":
+        return {"protocol": "ZIA++"}
+
+    if packet_id.lower().startswith("chacon"):
+        return {
+            "protocol": "chacon",
+            "address": packet_id.split(PACKET_ID_SEP)[1],
+        }
+
+    if packet_id.startswith("dooya_v4"):
+        return {
+            "protocol": "dooya_v4",
+            "id": packet_id.replace("dooya_v4_", "").split(PACKET_ID_SEP)[0],
+            "switch": packet_id.replace("dooya_v4_", "").split(PACKET_ID_SEP)[0],
+        }
+
+    packet_id_splited = packet_id.split(PACKET_ID_SEP)
+    packet = {
+        "protocol": packet_id_splited[0],
+        "id": packet_id_splited[1],
+    }
+    if len(packet_id_splited) > 2:
+        packet["switch"] = packet_id_splited[2]
+
+    return packet
+
+
+def packet_events(packet: PacketType) -> Generator[PacketType, None, None]:
+    """Handle packet events."""
+    field_abbrev = {
+        v: k
+        for k, v in sorted(
+            PACKET_FIELDS.items(), key=lambda x: (x[1], x[0]), reverse=True
+        )
+    }
+
+    packet_id = serialize_packet_id(packet)
+    # pour récup unit remise à none
+    unit = None
+    events = {f: v for f, v in packet.items() if f in field_abbrev}
+    for f, v in packet.items():
+        log.debug("f:%s,v:%s", f, v)
+#   Voir pour récup unité et type
+        if f == "unit":
+            unit = v
+        if f == "type":
+            type = v
+###
+    for s, v in events.items():
+        log.debug("event: %s -> %s", s, v)
+
+    for sensor, value in events.items():
+        log.debug("packet_events, sensor:%s,value:%s", sensor, value)
+    
+        yield {
+            "id": packet_id + PACKET_ID_SEP + field_abbrev[sensor],
+            "sensor": sensor,
+            "value": value,
+            "unit": unit,
+        }
 
 def deserialize_packet_id(packet_id: str) -> Dict[str, str]:
     """Deserialize packet id."""
