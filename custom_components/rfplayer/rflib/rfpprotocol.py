65b8bad3feff430cad590a94076aa77f5f50859d
Doubledom45 <97252459+Doubledom45@users.noreply.github.com>
2022-06-14 16:18:26 +0100

    Update rfpprotocol.py

--- custom_components/rfplayer/rflib/rfpprotocol.py
+++ custom_components/rfplayer/rflib/rfpprotocol.py
@@ -71,8 +71,7 @@ class ProtocolBase(asyncio.Protocol):
             if valid_packet(line):
                 self.handle_raw_packet(line)
             else:
-                log.warning("suppression des données invalides!", line)
-#                log.warning("dropping invalid data: %s", line)
+                log.warning("dropping invalid data: %s", line)
     def handle_raw_packet(self, raw_packet: str) -> None:
         """Handle one raw incoming packet."""
         raise NotImplementedError()
@@ -130,7 +129,7 @@ class PacketHandling(ProtocolBase):
                 else:
                     self.handle_packet(packet)
         else:
-            log.warning("Packet invalide ")
+            log.warning("no valid packet")
 
     def handle_packet(self, packet: PacketType) -> None:
         """Process incoming packet dict and optionally call callback."""
@@ -172,158 +171,6 @@ class PacketHandling(ProtocolBase):
 #####
         else:
             self.send_raw_packet(f"ZIA++{protocol} {command}")
-
-class CommandSerialization(PacketHandling):
-    """Logic for ensuring asynchronous commands are sent in order."""
-
-    def __init__(
-        self,
-        *args: Any,
-        packet_callback: Optional[Callable[[PacketType], None]] = None,
-        **kwargs: Any,
-    ) -> None:
-        """Add packethandling specific initialization."""
-        log.debug("CommandSerialization")
-        super().__init__(*args, **kwargs)
-        if packet_callback:
-            self.packet_callback = packet_callback
-        self._event = asyncio.Event(loop=self.loop)
-        self._lock = asyncio.Lock(loop=self.loop)
-
-    def handle_response_packet(self, packet: PacketType) -> None:
-        """Handle response packet."""
-        log.debug("handle_response_packet")
-        self._last_ack = packet
-        self._event.set()
-
-    async def send_command_ack(
-        self,
-        protocol: str,
-        command: str,
-        device_address: str = None,
-        device_id: str = None,
-    ) -> bool:
-        """Send command, wait for gateway to repond."""
-        async with self._lock:
-            self.send_command(protocol, command, device_address, device_id)
-            self._event.clear()
-            # await self._event.wait()
-        return True
-
-
-class EventHandling(PacketHandling):
-    """Breaks up packets into individual events with ids'.
-
-    Most packets represent a single event (light on, measured
-    temperature), but some contain multiple events (temperature and
-    humidity). This class adds logic to convert packets into individual
-    events each with their own id based on packet details (protocol,
-    switch, etc).
-    """
-
-    def __init__(
-        self,
-        *args: Any,
-        event_callback: Optional[Callable[[PacketType], None]] = None,
-        ignore: Optional[Sequence[str]] = None,
-        **kwargs: Any,
-    ) -> None:
-        """Add eventhandling specific initialization."""
-        super().__init__(*args, **kwargs)
-        self.event_callback = event_callback
-        # suppress printing of packets
-        log.debug("EventHandling")
-        if not kwargs.get("packet_callback"):
-            self.packet_callback = lambda x: None
-        if ignore:
-            log.debug("ignoring: %s", ignore)
-            self.ignore = ignore
-        else:
-            self.ignore = []
-
-    def _handle_packet(self, packet: PacketType) -> None:
-        """Event specific packet handling logic."""
-        events = packet_events(packet)
-
-        for event in events:
-            if self.ignore_event(event["id"]):
-                log.debug("ignoring event with id: %s", event)
-                continue
-            log.debug("got event: %s", event)
-            if self.event_callback:
-                self.event_callback(event)
-            else:
-                self.handle_event(event)
-
-    def handle_event(self, event: PacketType) -> None:
-        """Handle of incoming event (print)."""
-        log.debug("_handle_event")
-        string = "{id:<32} "
-        if "command" in event:
-            string += "{command}"
-        elif "version" in event:
-            if "hardware" in event:
-                string += "{hardware} {firmware} "
-            string += "V{version} R{revision}"
-        else:
-            string += "{value}"
-            if event.get("unit"):
-                string += " {unit}"
-
-        print(string.format(**event))
-
-    def handle_packet(self, packet: PacketType) -> None:
-        """Apply event specific handling and pass on to packet handling."""
-        self._handle_packet(packet)
-        super().handle_packet(packet)
-
-    def ignore_event(self, event_id: str) -> bool:
-        """Verify event id against list of events to ignore."""
-        log.debug("ignore_event")
-        for ignore in self.ignore:
-            if fnmatchcase(event_id, ignore):
-                return True
-        return False
-
-
-class RfplayerProtocol(CommandSerialization, EventHandling):
-    """Combine preferred abstractions that form complete Rflink interface."""
-
-
-def create_rfplayer_connection(
-    port: str,
-    baud: int = 115200,
-    protocol: Type[ProtocolBase] = RfplayerProtocol,
-    packet_callback: Optional[Callable[[PacketType], None]] = None,
-    event_callback: Optional[Callable[[PacketType], None]] = None,
-    disconnect_callback: Optional[Callable[[Optional[Exception]], None]] = None,
-    ignore: Optional[Sequence[str]] = None,
-    loop: Optional[asyncio.AbstractEventLoop] = None,
-) -> "Coroutine[Any, Any, Tuple[asyncio.BaseTransport, ProtocolBase]]":
-    """Create Rflink manager class, returns transport coroutine."""
-    if loop is None:
-        loop = asyncio.get_event_loop()
-    # use default protocol if not specified
-    protocol_factory = partial(
-        protocol,
-        loop=loop,
-        packet_callback=packet_callback,
-        event_callback=event_callback,
-        disconnect_callback=disconnect_callback,
-        ignore=ignore if ignore else [],
-    )
-
-    # setup serial connection
-    conn = create_serial_connection(loop, protocol_factory, port, baud)
-
-    return conn
-        if device_id is not None:
-            self.send_raw_packet(f"ZIA++{command} {protocol} ID {device_id}")
-        elif device_address is not None:
-            self.send_raw_packet(f"ZIA++{command} {protocol} {device_address}")
-        else:
-            self.send_raw_packet(f"ZIA++{protocol} {command}")
-
 
 class CommandSerialization(PacketHandling):
     """Logic for ensuring asynchronous commands are sent in order."""
