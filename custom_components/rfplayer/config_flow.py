bf1838d89f22450929644bc80edd9bb97d8489d5
Matthieu VIAL <12166434+racletteparty@users.noreply.github.com>
2025-09-13 18:19:14 +0000

    fix: solve config flow warning

--- custom_components/rfplayer/config_flow.py
+++ custom_components/rfplayer/config_flow.py
@@ -93,17 +93,13 @@ class RfplayerConfigFlow(ConfigFlow):
     @callback
     def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
         """Define the config flow to handle options."""
-        return RfPlayerOptionsFlowHandler(config_entry)
+        return RfPlayerOptionsFlowHandler()
 
 
 class RfPlayerOptionsFlowHandler(OptionsFlow):
     """Handle a RFPLayer options flow."""
 
     device_registry: dr.DeviceRegistry
-
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize."""
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> ConfigFlowResult:
         """Manage the options."""
